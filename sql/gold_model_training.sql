18e6200e85d8afed40ded3f6db3a9faf2c786de1
Happy Sibusiso Ndlovu <90577521+Babuse111@users.noreply.github.com>
2026-07-05 20:52:43 +0200

    Update gold_model_training.sql
    query update

--- sql/gold_model_training.sql
+++ sql/gold_model_training.sql
@@ -1,1 +1,12 @@
-
+CREATE OR REPLACE MODEL `incubeta-data-engineer.retail_gold.customer_segments_model`
+OPTIONS(
+  model_type = 'kmeans',
+  num_clusters = 4,
+  standardize_features = TRUE
+) AS
+SELECT
+  amount,
+  item_category
+FROM `incubeta-data-engineer.retail_silver.cleaned_transactions`
+WHERE amount IS NOT NULL
+  AND item_category IS NOT NULL;
