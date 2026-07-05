eee005938415418b17774b70f831803c2157b087
Happy Sibusiso Ndlovu <90577521+Babuse111@users.noreply.github.com>
2026-07-05 20:53:20 +0200

    Update gold_prediction.sql
    query update

--- sql/gold_prediction.sql
+++ sql/gold_prediction.sql
@@ -1,1 +1,18 @@
-
+CREATE OR REPLACE TABLE `incubeta-data-engineer.retail_gold.analytics_customer_segments` AS
+SELECT
+  transaction_id,
+  customer_id,
+  signup_date,
+  purchase_date,
+  amount,
+  item_category,
+  is_returned,
+  days_to_first_purchase,
+  CENTROID_ID AS predicted_cluster
+FROM ML.PREDICT(
+  MODEL `incubeta-data-engineer.retail_gold.customer_segments_model`,
+  (
+    SELECT *
+    FROM `incubeta-data-engineer.retail_silver.cleaned_transactions`
+  )
+);
