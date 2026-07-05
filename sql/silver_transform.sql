9f88e693f62795cae5360e4c88f9a39242e050ce
Happy Sibusiso Ndlovu <90577521+Babuse111@users.noreply.github.com>
2026-07-05 20:51:40 +0200

    Update silver_transform.sql
    quer update

--- sql/silver_transform.sql
+++ sql/silver_transform.sql
@@ -1,1 +1,17 @@
-
+CREATE OR REPLACE TABLE `incubeta-data-engineer.retail_silver.cleaned_transactions` AS
+SELECT
+  transaction_id,
+  customer_id,
+  COALESCE(SAFE_CAST(signup_date AS DATE), purchase_date) AS signup_date,
+  purchase_date,
+  SAFE_CAST(amount AS FLOAT64) AS amount,
+  TRIM(item_category) AS item_category,
+  COALESCE(SAFE_CAST(is_returned AS BOOL), FALSE) AS is_returned,
+  DATE_DIFF(
+    purchase_date,
+    COALESCE(SAFE_CAST(signup_date AS DATE), purchase_date),
+    DAY
+  ) AS days_to_first_purchase
+FROM `incubeta-data-engineer.retail_bronze.raw_transactions`
+WHERE SAFE_CAST(amount AS FLOAT64) > 0
+  AND purchase_date IS NOT NULL;
